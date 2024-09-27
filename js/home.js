6068085d7b9f127b281a8d99ca7b23879b4d9a87
Samiur <rsamiur247@gmail.com>
2024-09-28 00:06:15 +0600

    second

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log("home added")
+// add money to the account
+
+// step 1: add an event handler to the add money button inside the form
+// s-2 get money to be added to the account balance
+
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+// prevent page reload after form submit
+event.preventDefault();
+
+const addMoneyInput = document.getElementById('input-add-money').value;
+console.log(addMoneyInput);
+
+const pinNumberInput = document.getElementById('input-pin-number').value;
+console.log(pinNumberInput);
+});
